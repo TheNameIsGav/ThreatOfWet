96bd61bdd4c37fc741d0848e213e046449815045
mborenst <60239769+mborenst@users.noreply.github.com>
2021-11-08 12:12:14 -0600

    Some of the scale issues are fixed, player jump and dash local scale need to be corrected

--- Assets/scripts/Map Tile Scripts/MultiMovementTile.cs
+++ Assets/scripts/Map Tile Scripts/MultiMovementTile.cs
@@ -62,6 +62,7 @@ public class MultiMovementTile : BasicTile
             if (collision.gameObject.Equals(GameManager.instance.Player))
             {
                 collision.gameObject.transform.SetParent(transform);
+                collision.gameObject.transform.localScale = new Vector3(1/(transform.localScale.x), 1 / (transform.localScale.y), 1);
             }
         }
     }
@@ -73,6 +74,7 @@ public class MultiMovementTile : BasicTile
             if (collision.gameObject.Equals(GameManager.instance.Player))
             {
                 collision.gameObject.transform.SetParent(null);
+                collision.gameObject.transform.localScale = new Vector3(1, 1, 1);
             }
         }
     }
