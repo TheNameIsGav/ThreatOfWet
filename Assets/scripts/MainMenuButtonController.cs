cb0874e5a68288e8dc1c12170830816350a13d6e
Wright Gordon <mgordon@trinity.edu>
2021-12-03 01:51:19 -0600

    Added better looking main menu, with hover and click indications, as well as a cursor that moves with the mouse. I have not tested it with a controller yet, so I doubt that works.

--- Assets/scripts/MainMenuButtonController.cs
+++ Assets/scripts/MainMenuButtonController.cs
@@ -9,4 +9,9 @@ public class MainMenuButtonController : MonoBehaviour
     {
         SceneManager.LoadScene("WrightTutorialLevel");
     }
+
+    public void ExitGame()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
 }
---
+++ Assets/scripts/cursorController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorController : MonoBehaviour
+{
+    private Vector3 position1 = new Vector3 (-120, 64, 1);
+    private Vector3 position2 = new Vector3(-70, -48.5f, 0);
+    private Vector3 position3 = new Vector3(-44, -90, 0);
+    public Transform myCursor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void moveCursorStart()
+    {
+        myCursor.transform.localPosition = position1;
+    }
+
+    public void moveCursorOptions()
+    {
+        myCursor.transform.localPosition = position2;
+    }
+
+    public void moveCursorExit()
+    {
+        myCursor.transform.localPosition = position3;
+    }
+}
