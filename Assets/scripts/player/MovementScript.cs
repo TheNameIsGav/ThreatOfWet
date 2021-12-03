c77a31809e404951c4e54429fbccc558b78481a4
jfroehli <jfroehli@trinity.edu>
2021-12-03 15:49:38 -0600

    wall jumping detection WEAPONS ACTUALLY WORK

--- Assets/scripts/player/MovementScript.cs
+++ Assets/scripts/player/MovementScript.cs
@@ -97,6 +97,12 @@ public class IdleState : State
                     playerController.instance.transform.localScale = (new Vector3(.9f / (playerController.instance.transform.parent.transform.localScale.x),
                         1.1f / (playerController.instance.transform.parent.transform.localScale.y), 1f));
                 */
+                if (playerController.instance.wall)
+                {
+                    Debug.Log("YOU GO HELL NWO");
+                    //playerController.instance.transform.position = new Vector2(playerController.instance.transform.position.x +(3* playerController.instance.dir), playerController.instance.transform.position.y);
+                    playerController.instance.rbs.velocity = new Vector2(playerController.instance.rbs.velocity.x/* - (playerController.instance.dir * 25f)*/, playerController.instance.rbs.velocity.y * 1.5f);
+                }
                 playerController.instance.coyote = playerController.instance.universalBufferTime + 1;
             }
 
