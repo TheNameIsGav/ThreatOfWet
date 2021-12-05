883485bfba3adba61cdfd3406624f18d7292e6e5
mborenst <60239769+mborenst@users.noreply.github.com>
2021-12-05 15:21:58 -0600

    One Way Tile has been fully redone and testing shows much higher success and great performance

--- Assets/scripts/Map Tile Scripts/DirectionCollisionTile.cs
+++ Assets/scripts/Map Tile Scripts/DirectionCollisionTile.cs
@@ -2,60 +2,128 @@ using UnityEngine;
 
 public class DirectionCollisionTile : BasicTile
 {
-    public int blockDirection;
+    public string blockDirection;
     readonly int UP = 1; // F
     readonly int RIGHT = 2; // R
     readonly int DOWN = 3; // E
     readonly int LEFT = 4; // E
     // That spells free
-    BoxCollider2D extraHitbox;
+    EdgeCollider2D extraHitbox;
+    bool generatingEdge = false;
+    bool playerColliding = false;
 
     // CreditReport.com/baby
     protected override void Start()
     {
         base.Start();
-        if (blockDirection == 0)
-            blockDirection = 1;
-        extraHitbox = gameObject.AddComponent<BoxCollider2D>();
-        if (Up())
+        extraHitbox = gameObject.GetComponent<EdgeCollider2D>();
+        Debug.Log("Before: " + extraHitbox);
+        if (extraHitbox == null)
         {
-            extraHitbox.offset = new Vector2(extraHitbox.offset.x, extraHitbox.offset.y - .3f);
-            // extraHitbox.size = new Vector2(1.2f, 1);
-        } else if (Right())
-        {
-            extraHitbox.offset = new Vector2(extraHitbox.offset.x - .3f, extraHitbox.offset.y);
-            // extraHitbox.size = new Vector2(1, 1.2f);
+            // extraHitbox = gameObject.AddComponent<EdgeCollider2D>();
+            generatingEdge = true;
         }
-        else if (Down())
+        Debug.Log("After: " + extraHitbox);
+        /*
+        if (generatingEdge)
         {
-            extraHitbox.offset = new Vector2(extraHitbox.offset.x, extraHitbox.offset.y + .5f);
-            // extraHitbox.size = new Vector2(1.2f, 1);
+            if (Up())
+            {
+                Bounds boxBounds = hitbox.bounds;
+                extraHitbox.points = new Vector2[] {new Vector2(boxBounds.center.x - boxBounds.extents.x, boxBounds.center.y + boxBounds.extents.y),
+                    new Vector2(boxBounds.center.x + boxBounds.extents.x, boxBounds.center.y + boxBounds.extents.y)};
+            }
+            else if (Right())
+            {
+                extraHitbox.points = new Vector2[] {new Vector2(transform.position.x + hitbox.bounds., transform.position.y + boxBounds.extents.y),
+                    new Vector2(transform.position.x + boxBounds.extents.x, transform.position.y - boxBounds.extents.y)};
+            }
+            else if (Down())
+            {
+                Bounds boxBounds = hitbox.bounds;
+                extraHitbox.points = new Vector2[] {new Vector2(boxBounds.center.x - boxBounds.extents.x, boxBounds.center.y - boxBounds.extents.y),
+                    new Vector2(boxBounds.center.x + boxBounds.extents.x, boxBounds.center.y - boxBounds.extents.y)};
+            }
+            else // if (Left()) 
+            {
+                Bounds boxBounds = hitbox.bounds;
+                extraHitbox.points = new Vector2[] {new Vector2(boxBounds.center.x - boxBounds.extents.x, boxBounds.center.y + boxBounds.extents.y),
+                    new Vector2(boxBounds.center.x - boxBounds.extents.x, boxBounds.center.y - boxBounds.extents.y)};
+            }
         }
-        else // if (Left()) 
-        {
-            extraHitbox.offset = new Vector2(extraHitbox.offset.x + .3f, extraHitbox.offset.y);
-            // extraHitbox.size = new Vector2(1, 1.2f);
-        }
-        extraHitbox.isTrigger = true;
-        // this.extraHitbox = newCollider;
+        extraHitbox.isTrigger = false;
+        */
+        hitbox.isTrigger = true;
     }
 
     bool Up()
     {
-        return blockDirection == UP;
+        return blockDirection.ToUpper().Contains("UP");
     }
     bool Right()
     {
-        return blockDirection == RIGHT;
+        return blockDirection.ToUpper().Contains("RIGHT");
     }
     bool Down()
     {
-        return blockDirection == DOWN;
+        return blockDirection.ToUpper().Contains("DOWN");
     }
     bool Left()
     {
-        return blockDirection == LEFT;
+        return blockDirection.ToUpper().Contains("LEFT");
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == GameManager.instance.Player
+            && PlayerDirectionMatches())
+            hitbox.isTrigger = false;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject == GameManager.instance.Player)
+            hitbox.isTrigger = true;
+    }
+
+    private bool PlayerDirectionMatches()
+    {
+        Vector3 ppos = GameManager.instance.Player.transform.position;
+        float pw = GameManager.instance.Player.GetComponent<BoxCollider2D>().bounds.size.x / 2f;
+        float ph = GameManager.instance.Player.GetComponent<BoxCollider2D>().bounds.size.y / 2f;
+        Vector3 tpos = transform.position;
+        if (Up())
+        {
+            if (ppos.y > tpos.y // see if its up
+                && (ppos.x - pw <= tpos.x + (hitbox.bounds.size.x / 2f)
+                && ppos.x + pw >= tpos.x - (hitbox.bounds.size.x / 2f))) // see if its actually up
+                return true;
+        } 
+        if (Right())
+        {
+            if (ppos.x > tpos.x // see if its to the right
+                && (ppos.y - ph <= tpos.y + (hitbox.bounds.size.y / 2f)
+                && ppos.y + ph >= tpos.y - (hitbox.bounds.size.y / 2f))) // see if its actually to the right;
+                return true;
+        } 
+        if (Down())
+        {
+            return ppos.y < tpos.y // see if its down
+                && (ppos.x - pw <= tpos.x + (hitbox.bounds.size.x / 2f)
+                && ppos.x + pw >= tpos.x - (hitbox.bounds.size.x / 2f)); // see if its actually down
+        } 
+        if (Left())
+        {
+            return ppos.x < tpos.x // see if its to the left
+                && (ppos.y - ph <= tpos.y + (hitbox.bounds.size.y / 2f)
+                && ppos.y + ph >= tpos.y - (hitbox.bounds.size.y / 2f)); // see if its actually to the left;
+        } 
+        if (!Up() && !Down() && !Right() && !Left())
+            Debug.LogError("One Way Collider '" + name + "' does not have a valid direction (" + blockDirection + ")");
+        return false;
+    }
+
+    /*
 
     public override void updateBehavior(Collider2D col, Collider2D other, bool exitingCollider)
     {
@@ -106,4 +174,5 @@ public class DirectionCollisionTile : BasicTile
             Debug.Log("Entering block");
         }
     }
+    */
 }
